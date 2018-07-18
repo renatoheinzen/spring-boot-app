99b2ff3dc2c14da7da73fba221fe2f92932c4876
Renato Heinzen <renato.heinzen@outlook.com>
2018-07-18 20:17:57 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/unisul/tcc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.unisul.tcc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unisul.tcc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+
+}
