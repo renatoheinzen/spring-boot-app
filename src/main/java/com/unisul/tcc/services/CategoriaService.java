4e70f50ffc44376577b70df8373f412533a3e676
Renato Heinzen <renato.heinzen@outlook.com>
2018-07-19 20:08:54 -0300

    Listando as Categorias com CategoriaDTO

--- src/main/java/com/unisul/tcc/services/CategoriaService.java
+++ src/main/java/com/unisul/tcc/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.unisul.tcc.services;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,10 @@ public class CategoriaService {
 		}
 		
 		
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 	
 }
