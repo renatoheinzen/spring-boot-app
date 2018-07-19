ba33ffaf6f6deacf3b2127d70e5ae699c26e3fc7
Renato Heinzen <renato.heinzen@outlook.com>
2018-07-19 19:04:51 -0300

    Atualizando Categoria PUT, outros pequenos ajuste

--- src/main/java/com/unisul/tcc/resources/CategoriaResource.java
+++ src/main/java/com/unisul/tcc/resources/CategoriaResource.java
@@ -1,12 +1,9 @@
 package com.unisul.tcc.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +24,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) throws ObjectNotFoundException {
 
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok(obj);
@@ -41,5 +38,11 @@ public class CategoriaResource {
 				  .path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update (@PathVariable Integer id, @RequestBody Categoria obj) throws ObjectNotFoundException {
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+	}
 }
