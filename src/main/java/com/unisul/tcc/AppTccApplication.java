f0e51274c06b4e59692418bfb1708885ae247b86
Renato Heinzen <renato.heinzen@outlook.com>
2018-07-18 21:49:34 -0300

    Estado e Cidade

--- src/main/java/com/unisul/tcc/AppTccApplication.java
+++ src/main/java/com/unisul/tcc/AppTccApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.unisul.tcc.domain.Categoria;
+import com.unisul.tcc.domain.Cidade;
+import com.unisul.tcc.domain.Estado;
 import com.unisul.tcc.domain.Produto;
 import com.unisul.tcc.repositories.CategoriaRepository;
+import com.unisul.tcc.repositories.CidadeRepository;
+import com.unisul.tcc.repositories.EstadoRepository;
 import com.unisul.tcc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -24,6 +28,12 @@ public class AppTccApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -44,6 +54,20 @@ public class AppTccApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+		Estado est1= new Estado(null, "Minas Gerais");
+		Estado est2= new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 		
 	}
 }
---
+++ src/main/java/com/unisul/tcc/repositories/EstadoRepository.java
@@ -0,0 +1,14 @@
+package com.unisul.tcc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unisul.tcc.domain.Categoria;
+import com.unisul.tcc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+	
+
+}
