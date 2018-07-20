da3d66b5c4907383823dfc1860dad9c8598b035d
Renato Heinzen <renato.heinzen@outlook.com>
2018-07-19 21:13:12 -0300

    Paginção com parametros opicionais na requisição

--- src/main/java/com/unisul/tcc/services/CategoriaService.java
+++ src/main/java/com/unisul/tcc/services/CategoriaService.java
@@ -1,11 +1,13 @@
 package com.unisul.tcc.services;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.unisul.tcc.domain.Categoria;
@@ -51,6 +53,13 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
 	}
 	
 }
