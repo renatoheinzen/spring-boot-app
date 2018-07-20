9cdb30b9cbea51d73708b70e1eceaa1c71b87ee4
Renato Heinzen <renato.heinzen@outlook.com>
2018-07-20 20:08:17 -0300

    Validação customizada: Validação na atualização de cliente, email já existente

--- src/main/java/com/unisul/tcc/dto/ClienteDTO.java
+++ src/main/java/com/unisul/tcc/dto/ClienteDTO.java
@@ -8,8 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.unisul.tcc.domain.Cliente;
-import com.unisul.tcc.services.validation.ClienteInsert;
+import com.unisul.tcc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
