3c29b9ef6f1bccd36e9cc97bcd51f5b6d6b36872
Renato Heinzen <renato.heinzen@outlook.com>
2018-07-20 21:59:13 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/unisul/tcc/repositories/ProdutoRepository.java
+++ src/main/java/com/unisul/tcc/repositories/ProdutoRepository.java
@@ -1,11 +1,30 @@
 package com.unisul.tcc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.unisul.tcc.domain.Categoria;
 import com.unisul.tcc.domain.Produto;
 
 @Repository
+@Transactional(readOnly=true)
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	
+	/**
+	 * Using JPQL to construct the query or to construct the query by the name's rules.
+	 * @param nome
+	 * @param categorias
+	 * @param pageRequest
+	 * @return Page<Produto>
+	 */
+	@Query("select distinct obj from Produto obj inner join obj.categorias cat where obj.nome like %:nome% and cat in :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome,@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	
 }
---
+++ src/main/java/com/unisul/tcc/services/ProdutoService.java
@@ -0,0 +1,43 @@
+package com.unisul.tcc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.unisul.tcc.domain.Categoria;
+import com.unisul.tcc.domain.Produto;
+import com.unisul.tcc.repositories.CategoriaRepository;
+import com.unisul.tcc.repositories.ProdutoRepository;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) throws ObjectNotFoundException {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+								"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+		
+		
+	}
+
+}
