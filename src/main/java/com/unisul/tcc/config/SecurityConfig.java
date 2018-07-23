abfb3b8736ca747bfb35962ccb4336c35edf703e
Renato Heinzen <renato.heinzen@outlook.com>
2018-07-22 23:14:56 -0300

    Implementando autorizacao

--- src/main/java/com/unisul/tcc/config/SecurityConfig.java
+++ src/main/java/com/unisul/tcc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.unisul.tcc.security.JWTAuthenticationFilter;
+import com.unisul.tcc.security.JWTAuthorizationFilter;
 import com.unisul.tcc.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers(HttpMethod.GET,  PUBLIC_MACTHERS_GET).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
